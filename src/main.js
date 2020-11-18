2a5dd99e31c70fdfb9510a45369a22eb1c65b6c2
wannaSpring <wannaspring.2@gmail.com>
2020-11-19 01:13:45 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-import util from './utils/util'
-import Layout from '~/layouts/Layout.vue'
+import DefaultLayout from '~/layouts/Default.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueQuillEditor from 'vue-quill-editor'
@@ -10,9 +9,10 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
   Vue.use(ElementUI)
   Vue.use(VueQuillEditor)
-  Vue.prototype.$util = util
   Vue.mixin({
     data() {
       return {
@@ -20,6 +20,5 @@ export default function (Vue, { router, head, isClient }) {
       }
     }
   })
-  // Set default layout as a global component
-  Vue.component('Layout', Layout)
+
 }
